5346199be8068d5ca43d08de5fbeffe127ce7859
Peter John Manuel <peterjohn.manuel@gmail.com>
2016-05-22 07:17:02 +0200

    Refactored Code

--- js/helper.js
+++ js/helper.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var thumbnail = '<section class="thumbnail"></section>';
-var animalName = '<h3>%data%</h3>';
+var animalName = '<h3 class="animal-title">%data%</h3>';
 var animalImage = '<img src="%data%" alt="%dataAlt%">';
 var animalDetails = '<section class="animal-details"></section>';
 var animalFact = '<p class="animal-fact">%data%</p>';
 var animalAttr = `
-<ul>
+<ul class="fact-list">
     <li><span class="animal-attr">Scientific Name</span>: %dataScientificName%</li>
     <li><span class="animal-attr">Average Length</span>: %dataAverageLength%</li>
     <li><span class="animal-attr">Average Lifespan</span>: %dataAverageLifeSpan%</li>
